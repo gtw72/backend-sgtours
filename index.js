bd3118160adaa456eaabffb7a6f1cd3a138de5b9
gtw72 <fercarloshenrique@gmail.com>
2026-04-16 19:10:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -128,6 +128,7 @@ app.post("/user/linkplatform", UserController.linkPlatform);
 app.post("/user/unlinkplatform", UserController.unlinkPlatform);
 app.get("/shared/:version/:type", sendShared);
 app.post("/user/profile", UserController.getProfile);
+app.get("/user/profile", UserController.getProfile);
 app.post("/user-equipped-cosmetics/update", UserController.updateCosmetics);
 app.post("/user/cosmetics/addskin", UserController.addSkin);
 app.post("/user/cosmetics/setequipped", UserController.setEquippedCosmetic);
